911b5e061ef91b0a632859c1a6dfebf20b01b7d2
Carlisle Rainey <carlislerainey@gmail.com>
2023-10-23 11:41:21 -0400

    Add or2001

---
+++ R/or2001.R
@@ -0,0 +1,46 @@
+#' A data set on MID onset; illustrates logistic regression
+#'
+#' Oneal and Russett (2001) data set used in Rainey (2016) to illustrate compression in logistic regression models.
+#' These are the data to reproduce the logistic regression coefficients in Table 2 on p. 11 of the \href{https://static.cambridge.org/content/id/urn:cambridge.org:id:article:S204984701500059X/resource/name/S204984701500059Xsup001.pdf}{Appendix to Rainey (2016)}.
+#' Note that I've altered the coding of some variables, so the coefficients won't replicate exactly.
+#'
+#'
+#' @format A dyad-year data set.
+#' \describe{
+#' \item{\code{statea}}{the first state in the dyad}
+#' \item{\code{stateb}}{the second state in the dyad}
+#' \item{\code{stateaname}}{the name of first state in the dyad; created with countrycode}
+#' \item{\code{statebname}}{the second state in the dyad; created with countrycode}
+#' \item{\code{year}}{the year}
+#' \item{\code{dispute}}{a factor variable indicating the onset of conflict, specifically the onset of a militarized interstate dispute}
+#' \item{\code{allies}}{a factor variable indicating whether the members of the dyad are allies or not. Allies are linked by a mutual defense treaty, neutrality pact, or entente.}
+#' \item{\code{lcaprat2}}{the logged ratio of the stronger state's military capability index to that of the weaker member}
+#' \item{\code{contiguity}}{factor variable indicating whether the members of the dyad are contiguous or not}
+#' \item{\code{dem.lo}}{the smaller of the two Polity IV scores in the dyad}
+#' \item{\code{logdstab}}{the logged number of miles between the capitals}
+#' \item{\code{power}}{factor variable indicating whether the dyad is minor powers or at least one member is a great power.}
+#' }
+#'
+#'
+#'
+#'
+#' @examples
+#'
+#' # a simple example
+#'
+#' or <- crdata::or2001
+#'
+#' # glm version of their gee on pp. 314 with no product term
+#' m.noprod <- glm(dispute ~ allies + lcaprat2 + contiguity + dem.lo + logdstab + power,
+#'                 family = "binomial", data = or)
+#'
+#' # glm version of their gee on pp. 314 with no product terms
+#' m.prod <- glm(dispute ~ allies + lcaprat2 + contiguity + dem.lo*logdstab + power,
+#'               family = "binomial", data = or)
+#'
+#'
+#' # there are no non-contiguous minor powers in this data set
+#' table(or$contiguity, or$power)
+#'
+#'
+"or2001"
---
+++ data-raw/or2001/or2001-clean.R
@@ -0,0 +1,39 @@
+
+library(tidyverse)
+
+country_names <- countrycode::codelist_panel |>
+  select(cown, year, country.name = country.name.en) |>
+  glimpse()
+
+# Data available at
+#   http://pantheon.yale.edu/~brusset/io_dta.zip
+or2001 <- foreign::read.dta("data-raw/or2001/or.dta") |>
+  mutate(dem.lo = pmin(demauta, demautb)) |>
+  #mutate(stateaname = countrycode::countrycode(statea, origin = "cown", destination = "country.name")) |>
+  #mutate(statebname = countrycode::countrycode(stateb, origin = "cown", destination = "country.name")) |>
+  left_join(country_names, by = join_by(statea == cown, year == year)) |>
+  rename(stateaname = country.name) |>
+  left_join(country_names, by = join_by(stateb == cown, year == year)) |>
+  rename(statebname = country.name) |>
+  mutate(contiguity = case_when(noncontg == 0 ~ "Noncontiguous",
+                              noncontg == -1 ~ "Contiguous"),
+         contiguity = factor(contiguity)) |>
+  mutate(power = case_when(minrpwrs == 0 ~ "Minor Powers",
+                                minrpwrs == -1 ~ "At Least One Great Power"),
+         power = factor(power)) |>
+  mutate(allies = case_when(allies == 0 ~ "Not Allies",
+                            allies == 1 ~ "Allies"),
+         allies = factor(allies)) |>
+  mutate(dispute = case_when(dispute1 == 1 ~ "Dispute",
+                            dispute1 == 0 ~ "No Dispute"),
+         dispute = factor(dispute, levels = c("No Dispute", "Dispute"))) |>
+  select(stateaname, statebname, statea, stateb, year, dispute, dispute, allies, lcaprat2, contiguity,
+         dem.lo, logdstab, power) |>
+  glimpse()
+
+usethis::use_data(or2001, overwrite = TRUE)
+
+
+# write to inst/extdata
+write_csv(or2001, "inst/extdata/or2001.csv")
+write_rds(or2001, "inst/extdata/or2001.rds")
